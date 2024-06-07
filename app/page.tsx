168d6240e925b1b4db5545fcb4fb57696b0995e9
Daniel Molloy <danielmolloy_6@icloud.com>
2024-06-07 17:02:44 +0100

    added selected date text

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main data-testid="home-page" className="flex bg-white min-h-screen flex-col items-center justify-between">
       <div className="border-b shadow-sm border-black mt-0 w-full h-12 px-2 flex flex-row items-center justify-between">
         <h1 className="font-semibold text-xl ">React Diary</h1>
-        <select data-testid="select-box" className="border rounded m-2 p-1" onChange={(e) => setCalendarView(e.target.value)}>
+        <select value={calendarView} data-testid="select-box" className="border rounded m-2 p-1" onChange={(e) => setCalendarView(e.target.value)}>
           <option value={"picker"}>Date Picker</option>
           <option value={"month"}>Month Calendar</option>
         </select>
@@ -48,6 +48,8 @@ export default function Home() {
               id: 'event-2'
             },
               ]}/>}
+                      <p className="m-4">Selected date: {selectedDate.toLocaleString()}</p>
+
       </div>
       <div className=" w-full h-24 flex flex-col items-center justify-center bg-slate-100">
               <a data-testid="github-link" href="https://github.com/danmolloy/react-diary" target="_blank" className="flex flex-row items-center m-1 text-2xl hover:text-indigo-600 hover:underline">
